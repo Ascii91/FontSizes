fa88a264575c4d1739bae69800efeb2f06b1c669
Ascii91 <milos2906@gmail.com>
2015-04-05 19:47:41 +0200

    c

--- Bubble/src/Main.java
+++ Bubble/src/Main.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,7 +9,7 @@ public class Main
 
 	public static void main(String[] args) throws java.lang.Exception
 	{
-
+		int sdfadf;
 		float fontWidth = 0;
 		float fontHeight = 0;
 		float width = 0;
