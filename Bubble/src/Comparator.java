d63dec47357ed40e63860104d1ddf98380667ad1
Ascii91 <milos2906@gmail.com>
2015-04-06 16:12:38 +0200

    Fucked up

--- Bubble/src/Comparator.java
+++ Bubble/src/Comparator.java
@@ -8,10 +8,10 @@ public class Comparator
     public static void main(String[] args) throws IOException
     {
 
-        BufferedReader fileReader = new BufferedReader(new FileReader("new.txt"));
-        BufferedReader fileReader2 = new BufferedReader(new FileReader("bez.txt"));
-
-        for (int i = 0; i < 8100; i++)
+        BufferedReader fileReader = new BufferedReader(new FileReader("merge1.txt")); // staro
+        BufferedReader fileReader2 = new BufferedReader(new FileReader("merge2.txt"));// novo
+        int counter = 0;
+        for (int i = 0; i < 3990000; i++)
         {
 
             String value1 = fileReader.readLine();
@@ -20,14 +20,20 @@ public class Comparator
             double val1 = Double.parseDouble(value1);
             double val2 = Double.parseDouble(value2);
 
-       
-            if(val1<val2){ System.out.println(""+i+"*"+(val2-val1));
-            
-            //System.out.println("SA"+i+":"+val1);
-            //System.out.println("BEZ"+i+":"+val2);
+            if (val1 == val2)
+            {
+                counter++;
+                System.out.println("" + i + "*" + (val2 - val1));
             }
-            
-        }
+            else
+            {
+                // counter++;
 
+            }
+            // System.out.println("SA"+i+":"+val1);
+            // System.out.println("BEZ"+i+":"+val2);
+
+        }
+        System.out.println("Kraj " + counter);
     }
 }
--- Bubble/src/Main.java
+++ Bubble/src/Main.java
@@ -1,202 +1,204 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Main
 {
 
-	public static void main(String[] args) throws java.lang.Exception
-	{
-		int sdfadf;
-		float fontWidth = 0;
-		float fontHeight = 0;
-		float width = 0;
-		float height = 0;
-		String sentence = "";
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				System.in));
+    public static void main(String[] args) throws java.lang.Exception
+    {
 
-//		BufferedReader readerFile = new BufferedReader(new FileReader(
-//				"file.txt"));
+        float fontWidth = 0;
+        float fontHeight = 0;
+        float width = 0;
+        float height = 0;
+        String sentence = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		// PrintWriter writer = new PrintWriter("output.txt");
-		// sentence = readerFile.readLine();
-		// for (int x = 1; x < 1000; x++)
-		// {
-		// System.out.println("x" + x);
-		// for (int y = 1; y < 1000; y++)
-		// {
+        BufferedReader readerFile = new BufferedReader(new FileReader("file.txt"));
 
-		String line = "";
-		line = reader.readLine();
+        PrintWriter writer = new PrintWriter("output.txt");
+        sentence = readerFile.readLine();
+        for (float x = 1; x < 1000; x += 1)
+        {
+            System.out.println("x" + x);
+            for (float y = 1; y < 1000; y += 1)
+            {
 
-		width = Float.parseFloat(line.split(" ")[0]);
+                String line = "";
+                // line = reader.readLine();
 
-		height = (Float.parseFloat(line.split(" ")[1]));//
-		line = reader.readLine();
+                width = x;// Float.parseFloat(line.split(" ")[0]);
 
-		sentence = line;
+                height = y;// (Float.parseFloat(line.split(" ")[1]));//
+                // line = reader.readLine();
+                line = line.trim();
+                // sentence = line;
 
-		List<String> words = Arrays.asList(sentence.split(" "));
+                List<String> words = Arrays.asList(sentence.split(" "));
+                // System.out.println("SIZE" + words.size());
+                if (width == 0 || height == 0)
+                {
+                    System.out.println(0);
 
-		if (width == 0 || height == 0)
-		{
-			System.out.println(0);
+                }
+                else
+                {
 
-		}
-		else
-		{
+                    int maxWord = 0;
+                    for (String word : words)
+                    {
+                        if (word.length() > maxWord)
+                        {
+                            maxWord = word.length();
+                        }
+                    }
 
-		int maxWord = 0;
-		for (String word : words)
-		{
-			if (word.length() > maxWord)
-			{
-				maxWord = word.length();
-			}
-		}
+                    fontWidth = width / maxWord;
+                    fontHeight = 3.0f * fontWidth / 2.0f; // desired font height
+                    // words = mergeWords(words, maxWord);
+                    while (true)
+                    {
+                        // System.out.println("Words.size"+ words.size());
+                        // System.out.println("BEFORE: " + words.size());
+                        words = mergeWords(words, maxWord);
+                        // System.out.println("After: " + words.size());
+                        // System.out.println("Words.size after"+ words.size());
+                        // System.out.println("**"+fontHeight);
+                        if (fontHeight * words.size() - height <= 0.0001 && fontWidth * maxWord - width < 0.0001)
+                        {
+                            // System.out.println("BREAKPOINT1: " +words.size());
+                            break;// :D super
+                        }
+                        else
+                        {
 
-		fontWidth = width / maxWord;
-		fontHeight = 3.0f * fontWidth / 2.0f; // desired font height
-		words = mergeWords(words, maxWord);
-		while (true)
-		{
-			// System.out.println("Words.size"+ words.size());
-			words = mergeWords(words, maxWord);
-			// System.out.println("Words.size after"+ words.size());
-			// System.out.println("**"+fontHeight);
-			if (fontHeight * words.size() <= height)
-			{
-				// System.out.println("BREAKPOINT1: " +words.size());
-				break;// :D super
-			} else
-			{
+                            // mora se smanjiti font za koliko?
+                            // za malo samo ili za sledeci merge
 
-				// mora se smanjiti font za koliko?
-				// za malo samo ili za sledeci merge
+                            float faliNam = (fontHeight * words.size() - height) / words.size();
 
-				float faliNam = (fontHeight * words.size() - height)
-						/ words.size();
+                            int myRowLength = 1 + Main.minTwoSize(words);
 
-				int myRowLength = 1 + Main.minTwoSize(words);
-				maxWord = myRowLength;
+                            float smanjenje = fontHeight - (3.0f * (width / myRowLength) / 2.0f);
 
-				float smanjenje = fontHeight
-						- (3.0f * (width / myRowLength) / 2.0f);
+                            // ako nam fali manje od jednog merga onda cemo da
+                            // smanjimo za
 
-				// ako nam fali manje od jednog merga onda cemo da
-				// smanjimo za
+                            if (faliNam <= smanjenje)
+                            {
+                                fontHeight -= faliNam;
+                                fontWidth = 2.0f * fontHeight / 3.0f;
 
-				if (faliNam <= smanjenje)
-				{
-					fontHeight -= faliNam;
-					fontWidth = 2.0f * fontHeight / 3.0f;
-					break;
-				}
+                                break;
+                            }
 
-				fontWidth = width / myRowLength;
-				fontHeight = 3.0f * fontWidth / 2.0f;
+                            fontWidth = width / myRowLength;
+                            fontHeight = 3.0f * fontWidth / 2.0f;
+                            maxWord = myRowLength;
+                        }
 
-			}
+                    }
 
-		}
-		int tempMax = 0;
-		for (String str : words)
-		{
-			if (str.length() > tempMax)
-			{
-				tempMax = str.length();
-			}
-		}
+                    
+                    if(width == 4 && height ==1 ){System.out.println(""+fontHeight+"*"+words.size());
+                    for(String str:words){System.out.println(str);}
+                    
+                    }
+                //    if(fontHeight*words.size()*1.0f-height>0.0001){System.out.println("*****************");}
+                    // " MaWOrd:"+ maxWord +" WORDSIZE "+words.size());}
+                    // writer.println("W:" + width + " " + "H:" + height + " FH:" + fontHeight +" FW: "+fontWidth);
+                    // +" WORDSIZE "+words.size());
+                
+                    for (String word : words)
+                     {
+                     writer.println(word);
+                     }
+                    writer.println("W:" + width + " H:" + height + " " + fontHeight);
+                    writer.flush();
+                }
+            }
 
-		// if (tempMax * fontWidth > width
-		// || fontHeight * words.size() - height > 0.0001)
-		// {
-		// System.out.println("****************************");
-		// writer.println("Width:" + width + "  Height:" + height
-		// + " Size:" + fontHeight + " Words size:"
-		// + words.size() + " Razlika:"
-		// + (fontHeight * words.size() - height));
-		// }
+            // System.out.println(fontHeight);
+        }
+    }
 
-		// writer.println(fontHeight);
-		// writer.flush();
-		// }
-		// }
+    // nalazi sumu duzina dve najmanje susedne reci
+    public static int minTwoSize(List<String> words)
+    {
+        int minSum = 10001;
 
-		System.out.println(fontHeight);
-		}
-	}
+        for (int i = 0; i < words.size() - 1; i++)
+        {
+            if (words.get(i).length() + words.get(i + 1).length() < minSum)
+            {
+                minSum = words.get(i).length() + words.get(i + 1).length();
+            }
+        }
 
-	// nalazi sumu duzina dve najmanje susedne reci
-	public static int minTwoSize(List<String> words)
-	{
-		int minSum = 10001;
+        return minSum;
+    }
 
-		for (int i = 0; i < words.size() - 1; i++)
-		{
-			if (words.get(i).length() + words.get(i + 1).length() < minSum)
-			{
-				minSum = words.get(i).length() + words.get(i + 1).length();
-			}
-		}
+    // samo merguje
+    public static List<String> mergeWords(List<String> words, int initMax)
+    {
+        if (words.size() == 1)
+        {
+            return words;
+        }
+        while (true)
+        {
+            if (words.size() == 1)
+            {
+                return words;
+            }
+            List<String> initList = new ArrayList<String>();
+            boolean merged = false;
+            for (int i = 0; i < words.size() - 1; i++)
+            {
+                String word1 = words.get(i);
+                String word2 = words.get(i + 1);
+                String mergedWords = word1 + " " + word2;
+                if (mergedWords.length() > initMax)
+                {
 
-		return minSum;
-	}
+                    initList.add(word1);
+                    if (i == words.size() - 2)
+                    {
+                        initList.add(word2);
+                    }
 
-	// samo merguje
-	public static List<String> mergeWords(List<String> words, int initMax)
-	{
-		if (words.size() == 1)
-		{
-			return words;
-		}
-		while (true)
-		{
-			if (words.size() == 1)
-			{
-				return words;
-			}
-			List<String> initList = new ArrayList<String>();
-			boolean merged = false;
-			for (int i = 0; i < words.size() - 1; i++)
-			{
-				String word1 = words.get(i);
-				String word2 = words.get(i + 1);
-				String mergedWords = word1 + "*" + word2;
-				if (mergedWords.length() > initMax)
-				{
+                }
+                else
+                {
+                    merged = true;
+                    initList.add(mergedWords);
+                    i++;
+                    if (i == words.size() - 2)
+                    {
+                        initList.add(words.get(words.size() - 1));
+                    }
 
-					initList.add(word1);
-					if (i == words.size() - 2)
-					{
-						initList.add(word2);
-					}
+                }
 
-				} else
-				{
-					merged = true;
-					initList.add(mergedWords);
-					i++;
-					if (i == words.size() - 2)
-					{
-						initList.add(words.get(words.size() - 1));
-					}
+            }
 
-				}
+            words = initList;
+            if (!merged)
+            {
+                break;
+            }
+        }
 
-			}
+        return words;
+    }
 
-			words = initList;
-			if (!merged)
-			{
-				break;
-			}
-		}
-
-		return words;
-	}
+  
 
 }
